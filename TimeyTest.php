f4f33e32a373cda1f41466497c8bbfbf3abac013
Juan Orozco <juanthedesigner@gmail.com>
2012-11-28 09:02:14 -0500

    Add comments, update test page
    Added more comments. Updated the test page.

--- TimeyTest.php
+++ TimeyTest.php
@@ -1,40 +1,119 @@
+<!DOCTYPE html>
 <?php
+	include 'YeOldTimey.php';
 
-include 'YeOldTimey.php';
-//starts timer
-$newTimer = new YeOldenTimey;
-//pause
-sleep(3);
-//add lap
-$newTimer->laps('set', 1);
-//pause
-sleep(1);
-//add lap
-$newTimer->laps('set', 2);
-//pause again
-sleep(2);
+	//starts timer
+	$newTimer = new YeOldenTimey;
+	//pause
+	sleep(3);
+	//add lap
+	$newTimer->laps('set', 1);
+	//pause
+	sleep(1);
+	//add lap
+	$newTimer->laps('set', 2);
+	//pause again
+	sleep(2);
 
-//stop timer
-$totaltime = $newTimer->timer('get');
-                                               
-$i = 1;
-$laps = array();
-
-//display results
-echo '<p>Start time: ' . $newTimer->starttime . '</p>';
-    
-//get laps
-$laps = $newTimer->laps('get');
+	//stop timer
+	$totaltime = $newTimer->timer('get');
 
 
-foreach( $laps as $l )
-{
-	echo '<p>Lap #' . $i . ' time: ' . $l . ' and total difference from end time:' . $newTimer->laps('compare', $i) . '</p>';
-	$i++;
-}
+	//BUILD HTML
 
-echo '<p>End Time: ' . $newTimer->endtime . '</p>';
+	$i = 1;
+	$laps = array();
 
-echo '<p>Total time elapsed: ' . $totaltime . '</p>';
+	//set up output vars
+	$startHTML = 'Start time: ' . $newTimer->starttime;
+	$endHTML = 'End Time: ' . $newTimer->endtime;
+	$totalHTML = 'Total time elapsed: ' . $totaltime;
+	$lapsHTML = '';
+	$lapsStart = '<li>';
+	$lapsEnd = '</li>';
+	//get laps
+	$laps = $newTimer->laps('get');
+	foreach( $laps as $l )
+	{
+		$lapsHTML = $lapsHTML . $lapsStart . 'Lap #' . $i . ' time: ' . $l . ' and total difference from end time:' . $newTimer->laps('compare', $i) . $lapsEnd;
+		$i++;
+	}
+
+
 
 ?>
+<html>
+<head>
+	<title>Ye Olde Timey Test</title>
+	<meta charset="UTF-8">
+	<style> 
+		/* clear */
+		body {margin: 0; padding:0; outline:none; border:none;}
+
+		body {width: 60%; margin: 0 auto 0 auto;}
+
+	</style>
+</head>
+<body>
+	<h1>Ye Olde Timey - A Test</h1>
+	<p>Just a stupid simple tool to help time scripts.</p>
+
+	<h2>Instructions:</h2>
+	<div>
+		<p>The test consists of two parts, really.  The main timer and lap times.</p>
+		<h4>Timer:</h4>
+		<ul>
+			<li>
+				<p>Initializing: 
+					<pre>$newTimer = new YeOldenTimey;</pre>
+					This also starts the timer.  You can RESTART the timer after initializing by:
+					<pre>$newTimer->timer('restart');</pre>
+				</p>
+			</li>
+			<li>
+				<p>Stop the timer: 
+					<pre>$newTimer->timer('get');</pre>
+					This stops the timer and returns the end time.<br />
+					Restarting the timer will not clear the end time by default. You can force it, though:
+					<pre>$newTimer->timer('restart', true );</pre>
+				</p>
+			</li>
+		</ul>
+		<h4>Lap Times:</h4>
+		<ul>
+			<li>
+				<p>Trigger a lap time: 
+					<pre>$newTimer->laps('set', 1);</pre>
+					Both arguments are optional, but if you want to easily track lap times later, you should pass both.
+					The first argument is the action, in this case SET creates a new lap time.
+					The second argument is the name of the lap time. I used numbers in this example but you can use strings if you like.
+				</p>
+			</li>
+			<li>
+				<p>Get a lap time: 
+					<pre>$newTimer->timer('get', 1);</pre>
+					This returns the lap time saved with the name of 1.  Remember that whatever you passed as the name is how you get it back. <br />
+					If you did not pass a name when you set the lap time, you will have to manually go through the array which is retrieved like so:
+					<pre>$newTimer->timer('get');</pre>
+				</p>
+			</li>
+			<li>
+				<p>Compare a lap time: 
+					<pre>$newTimer->laps('compare', 1)</pre>
+					This compares the lap time to the end time and returns the result. If end time hasn't been set yet, it uses the current time.
+				</p>
+			</li>
+		</ul>
+	</div>
+	<h2>Test Output:</h2>
+	<ul>
+		<li><?php echo $startHTML; ?></li>
+		<?php echo $lapsHTML ?>
+		<li><?php echo $endHTML; ?></li>
+		<li><?php echo $totalHTML; ?></li>
+
+	</ul>
+
+
+</body>
+</html>
--- YeOldTimey.php
+++ YeOldTimey.php
@@ -1,5 +1,15 @@
 <?php  
 
+/** 
+ * Ye OldenTimey
+ * 
+ * A stupid simple tool that helps with timing php scripts.
+ * 
+ * 
+ * @author Juan Orozco <juanthedesigner@gmail.com> 
+ * @copyright 2012 Juan Orozco
+ */  
+
 class YeOldenTimey 
 {  
     public $starttime;
@@ -13,37 +23,38 @@ class YeOldenTimey
         $this->startTimer();
     } 
 
-    public function startTimer()  
+    private function startTimer( $reset = false )  
     {  
-        $this->starttime = microtime(true);  
+        $this->starttime = microtime(true); 
+        if  ( $reset == true ) $this->endtime = null;
     }
 
-    public function endTimer()
+    private function endTimer()
     {
     	$this->endtime = microtime(true);
     }
     //calculates start/end time difference
-    public function getTotalTime()
+    private function getTotalTime()
     {
     	$this->endTimer();
     	$this->totaltime = $this->endtime - $this->starttime;
     	return $this->totaltime;
     }
     //records a lap time
-    public function setLapTime( $name = '' )
+    private function setLapTime( $name = '' )
     {
     	$time = microtime(true);
     	if ( $name == '' OR empty($name) ) $this->laptimes[]=$time;
     	else $this->laptimes[$name] = $time;
     }
-    public function getLapTime( $name = '' )
+    //gets lap time by name, if no name passed, returns all as array
+    private function getLapTime( $name = '' )
     {
         if ( $name == '' OR empty($name) ) return $this->laptimes;
             else return $this->laptimes[ $name ];
-        
     }
 
-
+    //main lap time method
     public function laps( $action = 'set', $name = '' )
     {
         switch( $action )
@@ -53,33 +64,36 @@ class YeOldenTimey
             break;
             case 'set':
                 $this->setLapTime($name);
+                return true;
             break;
             case 'compare':
+                //compares to end time or to current time.
                 $laptime = $this->getLapTime($name);
                 $endtime = empty($this->endtime) ? microtime(true) : $this->endtime;
                 $totaltime = $endtime - $laptime;
-                //echo $endtime;
-                //echo $laptime;
                 return $totaltime;
             break;
             default:
             // default to set
                 $this->setLapTime($name);
-
+                return true;
             break;
         }
     }
-    //main method
-    public function timer( $action = 'lap' )
+
+    //main timer method
+    public function timer( $action = 'lap', $force = false )
     {
         switch( $action )
         {
             case 'restart':
                 //restarts timer
-                $this->startTimer();
+                $this->startTimer($force);
+                return true;
             break;
             case 'end':
                 $this->endTimer();
+                return true;
             break;
             case 'get':
                 return $this->getTotalTime();
@@ -87,7 +101,7 @@ class YeOldenTimey
             default:
             //trigger a lap
                 $this->laps();
-
+                return true;
             break;
         }
     }
